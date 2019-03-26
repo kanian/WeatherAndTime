f45bf0c71574ec4131fd7f778ba0ee6106fd6588
Patrick Assoa <kanian77@gmail.com>
2019-03-26 13:00:13 +0000

    refactor: move presentation in its own class

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const WeatherLocation_1 = require("./WeatherLocation");
-const CurrentWeather_1 = require("./CurrentWeather");
-let localConditions = [];
-const currentWeatherAndTime = dataArray => {
-    let answer = [];
-    for (var i = 0; i < dataArray.length; i++) {
-        const locationFectcher = new WeatherLocation_1.WeatherLocation(dataArray[i]);
-        const weatherFetcher = new CurrentWeather_1.CurrentWeather();
-        const res = locationFectcher.getGeolocation()
-            .then(weatherFetcher.getCurrentConditions.bind(weatherFetcher))
-            .then(show)
-            .then(saveAnswer);
-    }
-    return answer;
-};
-const saveAnswer = result => localConditions.push(result);
-const show = answer => {
-    console.log(`Location: ${answer.location}`);
-    console.log(`Current time: ${answer.currentTime}`);
-    console.log(`Current weather: ${answer.weather.weather}`);
-    console.log(`Current temperature: ${answer.weather.temperatureF}F`);
-    return answer;
-};
-const answer = currentWeatherAndTime([['Abidjan'], ['Minneapolis', '55403']]);
+const PresentCurrentWeatherAndTime_1 = require("./PresentCurrentWeatherAndTime");
+const weatherPresenter = new PresentCurrentWeatherAndTime_1.PresentCurrentWeatherAndTime([['Abidjan'], ['Minneapolis', '55403']]);
+//const answer = currentWeatherAndTime([['Abidjan'],['Minneapolis','55403']])
 //# sourceMappingURL=index.js.map
