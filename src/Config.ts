5fe267f6af9814d5a38cd702baeb6f8038aeebce
Patrick Assoa <kanian77@gmail.com>
2019-03-26 13:12:41 +0000

    style: indentation

--- src/Config.ts
+++ src/Config.ts
@@ -1,10 +1,10 @@
 
 const Config = class {
-    static clientId: string = 'QykD6PjbwwYB9BxajoYGz';
-    static clientSecret: string = 'muWNTZcVuVXi6ldPd1AKnxtWJ67lxObyMqNJDDul';
-    static currentConditionsAPIUrl: string = 'https://api.aerisapi.com/observations/closest'
-    static locationAPIUrl: string = 'https://api.aerisapi.com/places/search'
-    static locationAPIUrlPostal: string = 'https://api.aerisapi.com/places/postalcodes/'
+  static clientId: string = 'QykD6PjbwwYB9BxajoYGz';
+  static clientSecret: string = 'muWNTZcVuVXi6ldPd1AKnxtWJ67lxObyMqNJDDul';
+  static currentConditionsAPIUrl: string = 'https://api.aerisapi.com/observations/closest'
+  static locationAPIUrl: string = 'https://api.aerisapi.com/places/search'
+  static locationAPIUrlPostal: string = 'https://api.aerisapi.com/places/postalcodes/'
 }
 
 export { Config }
