14d94ca052df1ae80aee29f93a564b948aed4738
Patrick Assoa <kanian77@gmail.com>
2019-03-26 13:06:06 +0000

    chore: remove dangling comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import {PresentCurrentWeatherAndTime} from './PresentCurrentWeatherAndTime'
 const weatherPresenter = new PresentCurrentWeatherAndTime([['Abidjan'],['Minneapolis','55403']])
-//const answer = currentWeatherAndTime([['Abidjan'],['Minneapolis','55403']])
 
