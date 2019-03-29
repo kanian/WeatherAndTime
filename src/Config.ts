15adfed0c044e4fa0e23007a1cf3ddaf50e66edb
Patrick Assoa <kanian77@gmail.com>
2019-03-29 13:42:25 +0000

    use your own keys

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 
 const Config = class {
-  static clientId: string = 'QykD6PjbwwYB9BxajoYGz';
-  static clientSecret: string = 'muWNTZcVuVXi6ldPd1AKnxtWJ67lxObyMqNJDDul';
+  static clientId: string = '{you-aries-client-id-here}';
+  static clientSecret: string = '{you-client-secret-here}';
   static currentConditionsAPIUrl: string = 'https://api.aerisapi.com/observations/closest'
   static locationAPIUrl: string = 'https://api.aerisapi.com/places/search'
   static locationAPIUrlPostal: string = 'https://api.aerisapi.com/places/postalcodes/'
